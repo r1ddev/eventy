5fd9325acc7a7493b820d72b9c1154af26b3140d
ekivoka <sonya.sesofa@gmail.com>
2020-05-16 15:14:30 +0300

    litle route fix

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import './css/App.scss'
 import {
-	BrowserRouter as Router,
-	Switch,
-	Route,
-	Link
-} from "react-router-dom";
+	BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 
 function App() {
 	return (
 		<div className="App">
-			<p>asdasd</p>
 			<Link to="/profile/edit">Registration</Link>
 		</div>
 	);
