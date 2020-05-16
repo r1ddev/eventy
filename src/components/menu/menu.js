974683f495d36f4e9f37b104ade4728686e3f3f6
ekivoka <sonya.sesofa@gmail.com>
2020-05-16 19:42:50 +0300

    init menu

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import "./menu.css"
+
 class Menu extends React.Component {
+
+    state = {
+        itemsCount: 12
+    }
 
     render() {
         return (
             <div id="menu">
-                <ul>
-                    <li><Link to="/profile/edit">Регистрация</Link></li>
+                <MenuItem icon="lobby" label="Лобби" link="/lobby"></MenuItem>
+                <MenuItem icon="scenes" label="Сцены" link="/scenes"></MenuItem>
+                <MenuItem icon="program" label="Программа" link="/"></MenuItem>
+                <MenuItem icon="spikers" label="Спикеры" link="/spikers"></MenuItem>
+                <MenuItem icon="presentations" label="Презентации" link="/presentations"></MenuItem>
+                <MenuItem icon="lobby" label="Лобби" link="/lobby"></MenuItem>
+                <MenuItem icon="lobby" label="Лобби" link="/lobby"></MenuItem>
+                <MenuItem icon="lobby" label="Лобби" link="/lobby"></MenuItem>
+                <MenuItem icon="lobby" label="Лобби" link="/lobby"></MenuItem>
+                <MenuItem icon="lobby" label="Лобби" link="/lobby"></MenuItem>
+                <MenuItem icon="lobby" label="Лобби" link="/lobby"></MenuItem>
+                {/* <ul>
                     <li><Link to="/lobby">Лобби</Link></li>
                     <li><Link to="/presentations">Презентации</Link></li>
                     <li><Link to="/scenes">Сцены</Link></li>
@@ -18,7 +33,26 @@ class Menu extends React.Component {
                     <li><Link to="/conversations">Переговорки</Link></li>
                     <li><Link to="/party">Вечеринки</Link></li>
                     <li><Link to="/vip-assistent">Вип ассистент</Link></li>
-                </ul>
+                </ul> */}
+
+            </div>
+        )
+    }
+}
+
+
+
+class MenuItem extends React.Component {
+    render() {
+
+        const { label, link, icon } = this.props
+
+        return (
+            <div className="menu-item">
+                <Link to={link}>
+                    <img alt="" className="menu-item-icon" />
+                    <div className="menu-item-label" ><p>{label}</p></div>
+                </Link>
             </div>
         )
     }
