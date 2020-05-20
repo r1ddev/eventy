188d6ce8e599bb8e3aa9f4b550ef526b6abeb1a6
Eugene Dubenyuk <evgeniidubenyuk87@gmail.com>
2020-05-20 16:44:37 +0300

    page networking add api

--- src/pages/networking/networking.js
+++ src/pages/networking/networking.js
@@ -4,22 +4,63 @@ import withApiService from "../../components/hoc/with-api-service";
 import { connect } from "react-redux";
 import { compose } from "../../utils";
 import { Link } from 'react-router-dom';
+import api from './../../js/api';
+
 
 class Networking extends React.Component {
+	state = {
+		searchText: "",
+		users: []
+	};
+
+	searchSubmit = async (e) => {
+		e.preventDefault();
+	};
+
+	componentDidMount() {
+
+		this.fetchData()
+
+
+	}
+
+	fetchData = async () => {
+		console.log(this.props.user);
+
+		if (this.props.user.isLogin) {
+			api.account.getNetworking().then(res => {
+				this.setState({
+					users: res.user
+				})
+			}).catch(e => console.log(e))
+		} else {
+			this.props.history.push("/error")
+		}
+	}
+
 	render() {
+		const { users, searchText } = this.state;
 		return (
 			<div id="networking">
 				<div className="header profile-header">
 					<div className="container">
 						<div className="row">
 							<div className="col d-flex align-items-center">
-								<div className="search row m-0">
-									<input
-										type="text"
-										className="inp-search col"
-										placeholder="Search"
-									/>
-									<input type="submit" value="" className="btn-search" />
+								<div className="search">
+									<form
+										method="post"
+										className="row m-0"
+										onSubmit={this.searchSubmit}>
+										<input
+											type="text"
+											className="inp-search col"
+											placeholder="Search"
+											onChange={(e) => {
+												this.setState({ searchText: e.target.value });
+											}}
+										/>
+										<input type="submit" value="" className="btn-search" />
+									</form>
 								</div>
 							</div>
 
@@ -43,95 +84,39 @@ class Networking extends React.Component {
 				</div>
 				<div className="container">
 					<div className="card-list">
-						<div className="">
-							<div className="card">
-								<div className="row">
-									<div className="col-auto">
-										<div className="ava">
-											<img
-												src={require("../../images/networking-card-image-placeholder.png")}
-												alt=""
-											/>
+						{users.filter(user => {
+							return ~user.first_name.toLocaleLowerCase().indexOf(searchText.toLocaleLowerCase()) ||
+								~user.position.toLocaleLowerCase().indexOf(searchText.toLocaleLowerCase()) ||
+								~user.company.toLocaleLowerCase().indexOf(searchText.toLocaleLowerCase()) ||
+								~user.what_offer.toLocaleLowerCase().indexOf(searchText.toLocaleLowerCase()) ||
+								~user.what_looking.toLocaleLowerCase().indexOf(searchText.toLocaleLowerCase())
+						})
+							.map((user, index) => {
+								return (
+									<div className="" key={index}>
+										<div className="card">
+											<div className="row">
+												<div className="col-auto">
+													<div className="ava">
+														<img src={api.auth.getAvatarLocation() + user.avatar} alt="" />
+													</div>
+												</div>
+
+												<div className="col">
+													<div className="name">{user.first_name}</div>
+													<div className="desc">{user.position + " в " + user.company}</div>
+												</div>
+											</div>
+
+											<div className="title">Что предлагаю:</div>
+											<div className="text">{user.what_offer}</div>
+
+											<div className="title mt-5">Что ищу:</div>
+											<div className="text">{user.what_looking}</div>
 										</div>
 									</div>
-
-									<div className="col">
-										<div className="name">ЕВГЕНИЙ ДУБЕНЮК</div>
-										<div className="desc">Креативный директор Smit.events</div>
-									</div>
-								</div>
-
-								<div className="title">Что предлагаю:</div>
-								<div className="text">
-									Проконсультирую вас по ивентам (до 100 символов)
-								</div>
-
-								<div className="title mt-5">Что ищу:</div>
-								<div className="text">
-									Вдохновения, новые контакты. (до 100 символов)
-								</div>
-							</div>
-						</div>
-
-						<div className="">
-							<div className="card">
-								<div className="row">
-									<div className="col-auto">
-										<div className="ava">
-											<img
-												src={require("../../images/networking-card-image-placeholder.png")}
-												alt=""
-											/>
-										</div>
-									</div>
-
-									<div className="col">
-										<div className="name">ЕВГЕНИЙ ДУБЕНЮК</div>
-										<div className="desc">Креативный директор Smit.events</div>
-									</div>
-								</div>
-
-								<div className="title">Что предлагаю:</div>
-								<div className="text">
-									Проконсультирую вас по ивентам (до 100 символов)
-								</div>
-
-								<div className="title mt-5">Что ищу:</div>
-								<div className="text">
-									Вдохновения, новые контакты. (до 100 символов)
-								</div>
-							</div>
-						</div>
-
-						<div className="">
-							<div className="card">
-								<div className="row">
-									<div className="col-auto">
-										<div className="ava">
-											<img
-												src={require("../../images/networking-card-image-placeholder.png")}
-												alt=""
-											/>
-										</div>
-									</div>
-
-									<div className="col">
-										<div className="name">ЕВГЕНИЙ ДУБЕНЮК</div>
-										<div className="desc">Креативный директор Smit.events</div>
-									</div>
-								</div>
-
-								<div className="title">Что предлагаю:</div>
-								<div className="text">
-									Проконсультирую вас по ивентам (до 100 символов)
-								</div>
-
-								<div className="title mt-5">Что ищу:</div>
-								<div className="text">
-									Вдохновения, новые контакты. (до 100 символов)
-								</div>
-							</div>
-						</div>
+								);
+							})}
 					</div>
 				</div>
 			</div>
@@ -140,12 +125,14 @@ class Networking extends React.Component {
 }
 class NetworkingContainer extends React.Component {
 	render() {
-		return <Networking />;
+		return <Networking {...this.props} />;
 	}
 }
 
-const mapStateToProps = () => {
-	return {};
+const mapStateToProps = ({ user }) => {
+	return {
+		user,
+	};
 };
 
 const mapDispatchToProps = (dispatch, { apiService }) => {
