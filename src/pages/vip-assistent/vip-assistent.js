d2821c2a5ff6efb9d78f15e3981556f4790278e6
ekivoka <sonya.sesofa@gmail.com>
2020-05-25 16:53:44 +0300

    add vip-ass layout

--- src/pages/vip-assistent/vip-assistent.js
+++ src/pages/vip-assistent/vip-assistent.js
@@ -3,13 +3,31 @@ import './vip-assistent.css'
 import withApiService from '../../components/hoc/with-api-service'
 import { connect } from 'react-redux';
 import { compose } from '../../utils';
+import Header from '../../components/header/header';
+import ScenesChat from '../../components/scenes-chat/scenes-chat';
 
 class VipAssistent extends React.Component {
 
     render() {
         return (
-            <div>
-                Вип ассистент
+            <div id="vip-assistent">
+
+                <div className="ass-header"><Header /></div>
+
+                <div className="ass-info">
+                    <div className="ass-photo"></div>
+                    <div className="ass-label">Ваш персональный <br /> ассистент</div>
+                </div>
+
+                <div className="ass-chat">
+                    <ScenesChat
+                        loading={false}
+                        messages={[]}
+                        sendMessage={this.sendMessage}
+                        isPrivate={true}
+                        ref="scenesChat"
+                    />
+                </div>
             </div>
         )
     }
