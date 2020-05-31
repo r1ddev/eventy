eec57d21d5ae1b2045f2560d74a4df95bbb6ef5a
ekivoka <sonya.sesofa@gmail.com>
2020-05-31 17:40:11 +0300

    add header

--- src/pages/quest/quest.js
+++ src/pages/quest/quest.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import './quest.css'
+import Header from '../../components/header/header';
+import { Link } from 'react-router-dom';
 import withApiService from '../../components/hoc/with-api-service'
 import { connect } from 'react-redux';
 import { compose } from '../../utils';
+import { fetchUser } from '../../actions/user-actions';
 import ErrorIndicator from '../../components/error-indicator'
 import NoPermissions from '../../components/no-permissions';
 
@@ -11,6 +14,16 @@ class Quest extends React.Component {
     render() {
         return (
             <div id="quest">
+                <div className="quest-header">
+                    <Header data={null}>
+                        <></>
+                        <div className="col d-flex align-items-center p-0">
+                            <Link to="/messages/5" className="action-link">
+                                Связь <br />с организаторами
+                            </Link>
+                        </div>
+                    </Header>
+                </div>
                 <div className="quest-item">
                     <a href="#" style={{ backgroundImage: `url(${require("../../images/quests/1.svg")})` }}></a>
                 </div>
