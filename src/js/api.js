de03d6d87233df080efd53be337baec7027bb1b6
RidDev <evgeniidubenyuk87@gmail.com>
2020-07-06 18:21:22 +0300

    remove looking && offer

--- src/js/api.js
+++ src/js/api.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = {
 	// proxy: "https://cors-anywhere.herokuapp.com/",
@@ -15,87 +15,127 @@ const api = {
 	useAuth: () => {
 		return {
 			headers: {
-				Authorization: window.localStorage.token
-			}
-		}
+				Authorization: window.localStorage.token,
+			},
+		};
 	},
 	toFormData: (obj) => {
-		let fd = new FormData()
+		let fd = new FormData();
 
-		Object.keys(obj).map(key => {
-			fd.append(key, obj[key])
-		})
+		Object.keys(obj).map((key) => {
+			fd.append(key, obj[key]);
+		});
 
-		return fd
+		return fd;
 	},
 
 	auth: {
 		getUploadAvatarUrl() {
-			return "/users/avatar/upload"
+			return "/users/avatar/upload";
 		},
 		getAvatarLocation() {
-			return api.origin + "/images/avatar/"
+			return api.origin + "/images/avatar/";
 		},
-		registration: async (name, lastName, company, position, phone, email, soc, whatSearch, whatOffer, shareContact, tags) => {
-			let response = await axios.post(api.proxy + api.host + "/users/edit", api.toFormData({
-				first_name: name,
-				last_name: lastName,
-				company: company,
-				position: position,
-				phone: phone,
-				mail: email,
-				social_site: soc,
-				what_looking: whatSearch,
-				what_offer: whatOffer,
-				view_contact: (shareContact - 0),
-				tags: tags
-			}), api.useAuth())
+		registration: async (
+			name,
+			lastName,
+			company,
+			position,
+			phone,
+			email,
+			soc,
+			shareContact,
+			tags
+		) => {
+			let response = await axios.post(
+				api.proxy + api.host + "/users/edit",
+				api.toFormData({
+					first_name: name,
+					last_name: lastName,
+					company: company,
+					position: position,
+					phone: phone,
+					mail: email,
+					social_site: soc,
+					what_looking: "",
+					what_offer: "",
+					view_contact: shareContact - 0,
+					tags: tags,
+				}),
+				api.useAuth()
+			);
 
-			return response.data
-		}
+			return response.data;
+		},
 	},
 	account: {
 		async getUserData() {
-			let response = await axios.get(api.proxy + api.host + "/users/get", api.useAuth())
-			return response.data
+			let response = await axios.get(
+				api.proxy + api.host + "/users/get",
+				api.useAuth()
+			);
+			return response.data;
 		},
 		async getUserDataById(user_id) {
-			let response = await axios.get(api.proxy + api.host + "/users/public/get/" + user_id, api.useAuth())
-			return response.data
+			let response = await axios.get(
+				api.proxy + api.host + "/users/public/get/" + user_id,
+				api.useAuth()
+			);
+			return response.data;
 		},
 		async getNetworking() {
-			let response = await axios.get(api.proxy + api.host + "/users/public/all", api.useAuth())
-			return response.data
+			let response = await axios.get(
+				api.proxy + api.host + "/users/public/all",
+				api.useAuth()
+			);
+			return response.data;
 		},
 		messages: {
 			async getDialogs() {
-				let response = await axios.get(api.proxy + api.host + "/personal/messages", api.useAuth())
-				return response.data
+				let response = await axios.get(
+					api.proxy + api.host + "/personal/messages",
+					api.useAuth()
+				);
+				return response.data;
 			},
 			async getMessages(user_id) {
-				let response = await axios.get(api.proxy + api.host + "/personal/messages/from/" + user_id, api.useAuth())
-				return response.data
+				let response = await axios.get(
+					api.proxy + api.host + "/personal/messages/from/" + user_id,
+					api.useAuth()
+				);
+				return response.data;
 			},
 			async sendMessages(user_id, text) {
-				let response = await axios.post(api.proxy + api.host + "/personal/messages", api.toFormData({
-					user_id: user_id,
-					text: text
-				}), api.useAuth())
-				return response.data
+				let response = await axios.post(
+					api.proxy + api.host + "/personal/messages",
+					api.toFormData({
+						user_id: user_id,
+						text: text,
+					}),
+					api.useAuth()
+				);
+				return response.data;
 			},
 		},
 		conversations: {
 			async getRooms() {
-				let response = await axios.get(api.proxy + api.host + "/rooms/all", api.useAuth())
-				return response.data
+				let response = await axios.get(
+					api.proxy + api.host + "/rooms/all",
+					api.useAuth()
+				);
+				return response.data;
 			},
 			async updateRoomStatus(roomId) {
-				let response = await axios.post(api.proxy + api.host + "/rooms/booking", api.toFormData({
-					room_id: roomId,
-				}), api.useAuth())
-				return response.data
-			}
-		}
+				let response = await axios.post(
+					api.proxy + api.host + "/rooms/booking",
+					api.toFormData({
+						room_id: roomId,
+					}),
+					api.useAuth()
+				);
+				return response.data;
+			},
+		},
 	},
 
 	errorHandler: (e, errors) => {
@@ -103,14 +143,14 @@ const api = {
 			console.log("Ошибка Интернета");
 		} else {
 			Object.keys(errors).map(function (key) {
-				e.response.data.error = String(e.response.data.error)
+				e.response.data.error = String(e.response.data.error);
 
 				if (e.response.data.error === key) {
-					errors[key]()
+					errors[key]();
 				}
-			})
+			});
 		}
-	}
-}
+	},
+};
 
 export default api;
