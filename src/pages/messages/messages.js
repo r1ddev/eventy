62387091c33f94d038ea56bc355823f33eac3525
RidDev <evgeniidubenyuk87@gmail.com>
2020-07-10 23:22:16 +0300

    move logic to container

--- src/pages/messages/messages.js
+++ src/pages/messages/messages.js
@@ -15,193 +15,20 @@ import AES from "crypto-js/aes";
 import CryptoJS from "crypto-js";
 
 class Messages extends React.Component {
-	state = {
-		users: [],
-		activeUser: {},
-		messages: [],
-		loading: true,
-	};
-
-	updateTimer = 5000;
-	updateDialogsTimer = 20000;
-
-	timeoutMessages = undefined;
-	timeoutDialogs = undefined;
-
-	componentDidMount() {
-		this.fetchData();
-	}
-
-	fetchData = (setUser = true) => {
-		api.account.messages
-			.getDialogs()
-			.then((res) => {
-				res.dialogs = res.dialogs.filter((dialog) => {
-					return dialog.range != 6;
-				});
-				this.setState(
-					{
-						users: res.dialogs,
-					},
-					() => {
-						if (setUser) {
-							if (this.props.match.params.id) {
-								this.setUser(this.props.match.params.id);
-							}
-						}
-					}
-				);
-			})
-			.catch((e) => console.log(e));
-
-		clearTimeout(this.timeout);
-		this.timeoutDialogs = setTimeout(() => {
-			this.fetchData(false);
-		}, this.updateDialogsTimer);
-	};
-
-	asetState = (newState) => {
-		return new Promise((resolve, reject) => {
-			this.setState(newState, () => resolve());
-		});
-	};
-
-	setUser = async (userId) => {
-		this.setState({
-			loading: true,
-		});
-		let activeUser = this.state.users.find((u) => u.user_id == userId);
-
-		let unreadCount = this.state.users.filter((u) => u.read == 0).length;
-
-		if (unreadCount > 0) {
-			this.props.setMessagesNotifications(false);
-		}
-
-		if (activeUser !== undefined) {
-			activeUser.read = 1;
-			await this.asetState({
-				activeUser: activeUser,
-			});
-		} else {
-			let userData = await api.account.getUserDataById(userId);
-
-			await this.asetState({
-				activeUser: {
-					first_name: userData.user.first_name,
-					last_name: userData.user.last_name,
-					avatar: userData.user.avatar,
-					user_id: userId,
-					company: userData.user.company,
-					position: userData.user.position,
-					mail: userData.user.mail || "",
-					phone: userData.user.phone || "",
-					social_site: userData.user.social_site || "",
-					what_looking: userData.user.what_looking || "",
-					what_offer: userData.user.what_offer || "",
-				},
-			});
-		}
-
-		await this.fetchMessages(userId);
-
-		this.setState({
-			loading: false,
-		});
-
-		this.refs.scenesChat.onUpdate(true);
-	};
-
-	fetchMessages = async (userId) => {
-		let messages = await api.account.messages.getMessages(userId);
-
-		messages.messages = messages.messages.map((message) => {
-			if (window.localStorage.ckey != undefined) {
-				try {
-					let dec = AES.decrypt(
-						message.text,
-						window.localStorage.ckey || ""
-					);
-					message.text = dec.toString(CryptoJS.enc.Utf8);
-				} catch (error) {}
-			}
-			return { ...message, message: message.text };
-		});
-
-		await this.asetState({
-			messages: messages.messages,
-		});
-
-		this.refs.scenesChat.onUpdate(true);
-
-		clearTimeout(this.timeoutMessages);
-		this.timeoutMessages = setTimeout(() => {
-			this.fetchMessages(userId);
-		}, this.updateTimer);
-	};
-
-	componentDidUpdate(prevProps) {
-		if (prevProps.timers !== this.props.timers) {
-			this.updateTimer = this.props.timers.updateTimer;
-			this.updateDialogsTimer = this.props.timers.updateDialogsTimer;
-		}
-	}
-
-	sendMessage = (message) => {
-		let encMessage = "";
-
-		if (window.localStorage.ckey != undefined) {
-			let enc = AES.encrypt(message, window.localStorage.ckey || "");
-			encMessage = enc.toString();
-		}
-
-		api.account.messages
-			.sendMessages(this.state.activeUser.user_id, encMessage || message)
-			.then((res) => {})
-			.catch((e) => console.log(e));
-
-		let m = this.state.messages;
-		m.push({
-			first_name: this.props.user.data.first_name,
-			last_name: this.props.user.data.last_name,
-			range: this.props.user.data.range,
-			message: message,
-			avatar: this.props.user.data.avatar,
-		});
-
-		this.setState(
-			{
-				messages: m,
-			},
-			() => {
-				this.refs.scenesChat.onUpdate(true);
-			}
-		);
-	};
-
-	componentWillUnmount() {
-		clearTimeout(this.timeoutDialogs);
-		clearTimeout(this.timeoutMessages);
-	}
-
-	onActive = async (e) => {
-		this.updateTimer = this.props.timers.updateTimer;
-		this.updateDialogsTimer = this.props.timers.updateDialogsTimer;
-	};
-
-	onIdle = (e) => {
-		this.updateTimer = 80000;
-		this.updateDialogsTimer = 80000;
-	};
-
 	render() {
+		// console.log("this.props", this.props);
+		// return <></>;
 		const {
 			users,
 			activeUser,
 			messages,
 			loading: messagesIsLoading,
-		} = this.state;
-		const { data } = this.props.user;
+			user: data,
+			onActive,
+			onIdle,
+			setUser,
+			sendMessage,
+		} = this.props;
 
 		var isChatAvailable = Object.entries(activeUser).length > 0;
 
@@ -213,15 +40,12 @@ class Messages extends React.Component {
 			activeUser.what_looking ||
 			false;
 
-		console.log("isContactAvailable", isContactAvailable);
-		console.log("messagesIsLoading", messagesIsLoading);
-
 		return (
 			<div id="messages">
 				<IdleTimer
 					element={document}
-					onActive={this.onActive}
-					onIdle={this.onIdle}
+					onActive={onActive}
+					onIdle={onIdle}
 					timeout={1000 * 30}
 				/>
 
@@ -252,9 +76,7 @@ class Messages extends React.Component {
 													className="user"
 													key={index}
 													onClick={() => {
-														this.setUser(
-															user.user_id
-														);
+														setUser(user.user_id);
 													}}>
 													<div>
 														<div className="row align-items-center">
@@ -290,7 +112,7 @@ class Messages extends React.Component {
 											<ScenesChat
 												loading={messagesIsLoading}
 												messages={messages}
-												sendMessage={this.sendMessage}
+												sendMessage={sendMessage}
 												isPrivate={true}
 												ref="scenesChat"
 											/>
@@ -364,8 +186,201 @@ class Messages extends React.Component {
 	}
 }
 class MessagesContainer extends React.Component {
+	state = {
+		users: [],
+		activeUser: {},
+		messages: [],
+		loading: true,
+	};
+
+	updateTimer = 5000;
+	updateDialogsTimer = 20000;
+
+	timeoutMessages = undefined;
+	timeoutDialogs = undefined;
+
+	componentDidMount() {
+		this.fetchData();
+	}
+
+	fetchData = (setUser = true) => {
+		api.account.messages
+			.getDialogs()
+			.then((res) => {
+				console.log("res", res);
+
+				res.dialogs = res.dialogs.filter((dialog) => {
+					return dialog.range != 6;
+				});
+				this.setState(
+					{
+						users: res.dialogs,
+					},
+					() => {
+						if (setUser) {
+							if (this.props.match.params.id) {
+								this.setUser(this.props.match.params.id);
+							}
+						}
+					}
+				);
+			})
+			.catch((e) => console.log(e));
+
+		clearTimeout(this.timeout);
+		this.timeoutDialogs = setTimeout(() => {
+			this.fetchData(false);
+		}, this.updateDialogsTimer);
+	};
+
+	setUser = async (userId) => {
+		this.setState({
+			loading: true,
+		});
+		let activeUser = this.state.users.find((u) => u.user_id == userId);
+
+		let unreadCount = this.state.users.filter((u) => u.read == 0).length;
+
+		if (unreadCount > 0) {
+			this.props.setMessagesNotifications(false);
+		}
+
+		if (activeUser !== undefined) {
+			activeUser.read = 1;
+			await this.asetState({
+				activeUser: activeUser,
+			});
+		} else {
+			let userData = await api.account.getUserDataById(userId);
+
+			await this.asetState({
+				activeUser: {
+					first_name: userData.user.first_name,
+					last_name: userData.user.last_name,
+					avatar: userData.user.avatar,
+					user_id: userId,
+					company: userData.user.company,
+					position: userData.user.position,
+					mail: userData.user.mail || "",
+					phone: userData.user.phone || "",
+					social_site: userData.user.social_site || "",
+					what_looking: userData.user.what_looking || "",
+					what_offer: userData.user.what_offer || "",
+				},
+			});
+		}
+
+		await this.fetchMessages(userId);
+
+		this.setState({
+			loading: false,
+		});
+
+		// this.refs.scenesChat.onUpdate(true);
+	};
+
+	asetState = (newState) => {
+		return new Promise((resolve, reject) => {
+			this.setState(newState, () => resolve());
+		});
+	};
+
+	fetchMessages = async (userId) => {
+		let messages = await api.account.messages.getMessages(userId);
+
+		messages.messages = messages.messages.map((message) => {
+			if (window.localStorage.ckey != undefined) {
+				try {
+					let dec = AES.decrypt(
+						message.text,
+						window.localStorage.ckey || ""
+					);
+					message.text = dec.toString(CryptoJS.enc.Utf8);
+				} catch (error) {}
+			}
+			return { ...message, message: message.text };
+		});
+
+		await this.asetState({
+			messages: messages.messages,
+		});
+
+		//this.refs.scenesChat.onUpdate(true);
+
+		clearTimeout(this.timeoutMessages);
+		this.timeoutMessages = setTimeout(() => {
+			this.fetchMessages(userId);
+		}, this.updateTimer);
+	};
+
+	componentDidUpdate(prevProps) {
+		if (prevProps.timers !== this.props.timers) {
+			this.updateTimer = this.props.timers.updateTimer;
+			this.updateDialogsTimer = this.props.timers.updateDialogsTimer;
+		}
+	}
+
+	sendMessage = (message) => {
+		let encMessage = "";
+
+		if (window.localStorage.ckey != undefined) {
+			let enc = AES.encrypt(message, window.localStorage.ckey || "");
+			encMessage = enc.toString();
+		}
+
+		api.account.messages
+			.sendMessages(this.state.activeUser.user_id, encMessage || message)
+			.then((res) => {})
+			.catch((e) => console.log(e));
+
+		let m = this.state.messages;
+		m.push({
+			first_name: this.props.user.data.first_name,
+			last_name: this.props.user.data.last_name,
+			range: this.props.user.data.range,
+			message: message,
+			avatar: this.props.user.data.avatar,
+		});
+
+		this.setState(
+			{
+				messages: m,
+			},
+			() => {
+				// this.refs.scenesChat.onUpdate(true);
+			}
+		);
+	};
+
+	componentWillUnmount() {
+		clearTimeout(this.timeoutDialogs);
+		clearTimeout(this.timeoutMessages);
+	}
+
+	onActive = async (e) => {
+		this.updateTimer = this.props.timers.updateTimer;
+		this.updateDialogsTimer = this.props.timers.updateDialogsTimer;
+	};
+
+	onIdle = (e) => {
+		this.updateTimer = 80000;
+		this.updateDialogsTimer = 80000;
+	};
+
 	render() {
-		return <Messages {...this.props} />;
+		return (
+			<Messages
+				users={this.state.users}
+				activeUser={this.state.activeUser}
+				messages={this.state.messages}
+				loading={this.state.messagesIsLoading}
+				onActive={this.onActive}
+				onIdle={this.onIdle}
+				setUser={this.setUser}
+				sendMessage={this.sendMessage}
+				{...this.props}
+			/>
+		);
 	}
 }
 
