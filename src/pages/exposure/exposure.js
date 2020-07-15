7b613ed13695b2ef13999f8bfb2ed836012779ba
RidDev <evgeniidubenyuk87@gmail.com>
2020-07-16 00:45:55 +0300

    mobilka exposure

--- src/pages/exposure/exposure.js
+++ src/pages/exposure/exposure.js
@@ -4,32 +4,32 @@ import withApiService from "../../components/hoc/with-api-service";
 import { connect } from "react-redux";
 import { compose } from "../../utils";
 import Header from "../../components/header";
-import { Link } from 'react-router-dom';
-import { isMobile } from 'react-device-detect';
-
+import { Link } from "react-router-dom";
+import { isMobile } from "react-device-detect";
 
 const partners = [
 	{
 		logo: require("../../images/partners/smitstudio.png"),
 		background: require("../../images/partners/smitstudio-bg.png"),
-		backgroundPosition: 'top',
+		backgroundPosition: "top",
 		name: "Smit.Studio",
-		desc:
-			(<p>
-
-				"SMIT.studio - эксперты в интерактивом маркетинге <br/> Наращивайте своё сообщество лояльными игроками, которых в будущем конвертируете в адвокатов бренда"
+		desc: (
+			<p>
+				"SMIT.studio - эксперты в интерактивом маркетинге <br />{" "}
+				Наращивайте своё сообщество лояльными игроками, которых в
+				будущем конвертируете в адвокатов бренда"
 			</p>
 		),
-				link: "https://smit.studio/"
+		link: "https://smit.studio/",
 	},
 	{
 		logo: require("../../images/partners/smitscreen.png"),
 		background: require("../../images/partners/smitscreen-bg.png"),
-		backgroundPosition: 'center',
+		backgroundPosition: "center",
 		name: "Smit.Screen",
 		desc:
 			"Проект, открывающий для бизнеса новые возможности в мире наружной рекламы",
-		link: "https://smitscreen.ru"
+		link: "https://smitscreen.ru",
 	},
 ];
 
@@ -37,17 +37,19 @@ class Exposure extends React.Component {
 	render() {
 		let partnersList = partners.map((partner) => {
 			return (
-
-				<a style={{
-					backgroundImage: `url(${partner.background})`, backgroundSize: 'cover', backgroundPosition: partner.backgroundPosition, backgroundRepeat: 'norepeat'
-				}}
+				<a
+					style={{
+						backgroundImage: `url(${partner.background})`,
+						backgroundSize: "cover",
+						backgroundPosition: partner.backgroundPosition,
+						backgroundRepeat: "norepeat",
+					}}
 					target="_blank"
 					href={partner.link}
 					className="card"
-					onClick={() => this.props.postUrl(partner.link)}
-				>
+					onClick={() => this.props.postUrl(partner.link)}>
 					<div className="row head m-0">
-						<div className="col-5 logo flex-center">
+						<div className="col-sm-5 logo flex-center">
 							<img src={partner.logo} alt="" />
 						</div>
 					</div>
@@ -58,16 +60,16 @@ class Exposure extends React.Component {
 
 		return (
 			<div id="exposure">
-
-				{(!isMobile)&&<Header className="fixed" data={this.props.user.data}>
-					<></>
-					<div className="col d-flex align-items-center p-0">
-						<Link to="/messages/5" className="action-link">
-							Связь <br />с организаторами
-						</Link>
-					</div>
-				</Header>
-				}
+				{!isMobile && (
+					<Header className="fixed" data={this.props.user.data}>
+						<></>
+						<div className="col d-flex align-items-center p-0">
+							<Link to="/messages/5" className="action-link">
+								Связь <br />с организаторами
+							</Link>
+						</div>
+					</Header>
+				)}
 				<div className="container">
 					<div className="card-list">{partnersList}</div>
 				</div>
@@ -90,7 +92,9 @@ const mapStateToProps = ({ user }) => {
 
 const mapDispatchToProps = (dispatch, { apiService }) => {
 	return {
-		postUrl: (url) => { apiService.postUrl(url) }
+		postUrl: (url) => {
+			apiService.postUrl(url);
+		},
 	};
 };
 
