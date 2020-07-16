5042f274c39eb49d0f54c698a5085be0a18d9979
RidDev <evgeniidubenyuk87@gmail.com>
2020-07-16 12:40:00 +0300

    up localizations

--- src/index.js
+++ src/index.js
@@ -1,30 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-import App from './App';
+import App from "./App";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { IdeaFirstApiService } from './services';
-import { ApiServiceProvider } from './components/api-service-context';
-import store from './store';
-import { Provider } from 'react-redux';
+import { IdeaFirstApiService } from "./services";
+import { ApiServiceProvider } from "./components/api-service-context";
+import store from "./store";
+import { Provider } from "react-redux";
 
-import EditProfile from './pages/edit-profile';
-import Profile from './pages/profile';
-import Error from './pages/error';
-import Login from './pages/login';
-import Registration from './pages/registration';
-import RegistrationAcception from './pages/registration-acception';
+import EditProfile from "./pages/edit-profile";
+import Profile from "./pages/profile";
+import Error from "./pages/error";
+import Login from "./pages/login";
+import Registration from "./pages/registration";
+import RegistrationAcception from "./pages/registration-acception";
 
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import PasswordRecovery from './pages/password-recovery';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import PasswordRecovery from "./pages/password-recovery";
+
+import { I18nextProvider } from "react-i18next";
+import i18n from "i18next";
+import i18nLocales from "./i18n";
 
 const apiService = new IdeaFirstApiService();
 
 class Index extends React.Component {
-
-
 	render() {
 		return (
 			<Provider store={store}>
@@ -40,23 +42,20 @@ class Index extends React.Component {
 							<Route path="/registration-acception" component={RegistrationAcception} />
 							<Route path="/password-recovery" component={PasswordRecovery} />
 
-
-
 							<Route path="/error" component={Error} />
 							<Route path="/" component={App} />
-
 						</Switch>
 					</Router>
-
 				</ApiServiceProvider>
 			</Provider>
-		)
+		);
 	}
 }
 
-
 ReactDOM.render(
-	<Index />
+	<I18nextProvider i18n={i18nLocales}>
+		<Index />
+	</I18nextProvider>,
 
-	, document.getElementById('root')
+	document.getElementById("root")
 );
--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -5,48 +5,51 @@ import { compose } from "../../utils";
 
 import "./login.scss";
 import { Link } from "react-router-dom";
-import ErrorIndicator from '../../components/error-indicator'
+import ErrorIndicator from "../../components/error-indicator";
 import Spinner from "../../components/spinner";
 
-class Login extends React.Component {
+import { withTranslation } from "react-i18next";
+import i18next from "i18next";
 
+class Login extends React.Component {
 	state = {
-		email: '',
-		password: '',
+		email: "",
+		password: "",
 		disableForm: false,
 		loading: false,
-
-	}
+	};
 
 	onChangeEmail = (e) => {
 		this.setState({
-			email: e.target.value
-		})
-	}
+			email: e.target.value,
+		});
+	};
 
 	onChangePassword = (e) => {
 		this.setState({
-			password: e.target.value
-		})
-	}
+			password: e.target.value,
+		});
+	};
 
 	onLoading = (status) => {
 		this.setState({
-			loading: status
-		})
-	}
+			loading: status,
+		});
+	};
 
 	onDisableForm = (status) => {
 		this.setState({
-			disableForm: status
-		})
-	}
+			disableForm: status,
+		});
+	};
 
 	saveToken = (token) => {
 		window.localStorage.token = token;
-	}
+	};
 
 	onSubmit = (e) => {
+		i18next.changeLanguage("en");
+
 		e.preventDefault();
 
 		let user = {
@@ -54,52 +57,48 @@ class Login extends React.Component {
 			password: this.state.password,
 		};
 
-
 		this.onLoading(true);
 		this.onDisableForm(true);
 
-		this.props.autorizate(user)
+		this.props
+			.autorizate(user)
 			.then((res) => {
-
 				this.onLoading(false);
 				this.onDisableForm(false);
-				this.saveToken(res.token)
+				this.saveToken(res.token);
 
 				if (res.myfirsttime) {
-					this.props.history.push("/profile/edit")
+					this.props.history.push("/profile/edit");
 				} else {
-					this.props.history.push("/desk")
+					this.props.history.push("/desk");
 				}
-
 			})
-			.catch(err => {
-
-				if (err.response.data.error == 'user_not_found') {
-					ErrorIndicator('Пользователя с этими данными не существует');
-				};
-
+			.catch((err) => {
+				if (err.response.data.error == "user_not_found") {
+					ErrorIndicator("Пользователя с этими данными не существует");
+				}
 
 				this.onLoading(false);
 				this.onDisableForm(false);
-			})
-	}
+			});
+	};
 
 	onLoginGuest = () => {
-		this.props.addGuestUser()
+		this.props
+			.addGuestUser()
 			.then((res) => {
 				this.saveToken(res.token);
-				this.props.history.push("/desk")
-
+				this.props.history.push("/desk");
 			})
-			.catch(
-				err => {
-					ErrorIndicator('Пользователя с этими данными не существует');
-				}
-			)
-	}
+			.catch((err) => {
+				ErrorIndicator("Пользователя с этими данными не существует");
+			});
+	};
 
 	render() {
+		console.log("this.props.t", this.props);
 
+		const t = this.props.t;
 		const { email, password, disableForm, loading } = this.state;
 
 		return (
@@ -107,21 +106,39 @@ class Login extends React.Component {
 				<form onSubmit={this.onSubmit} className="login-form">
 					<div className="login-form--wrapper">
 						<div className="login-form--caption">Авторизация</div>
-						<input required type="email" value={email} onChange={this.onChangeEmail} className="email-input" placeholder="e-mail"></input>
-						<input required type="password" value={password} onChange={this.onChangePassword} className="password-input" placeholder="Пароль"></input>
-						<button disabled={email == '' || password == '' || disableForm} className="white-button login-btn">
-							{(loading) ? <Spinner /> : "ВОЙТИ"}
+						<input
+							required
+							type="email"
+							value={email}
+							onChange={this.onChangeEmail}
+							className="email-input"
+							placeholder="e-mail"></input>
+						<input
+							required
+							type="password"
+							value={password}
+							onChange={this.onChangePassword}
+							className="password-input"
+							placeholder="Пароль"></input>
+						<button
+							disabled={email == "" || password == "" || disableForm}
+							className="white-button login-btn">
+							{loading ? <Spinner /> : "ВОЙТИ"}
 						</button>
 
-						<button type='button' className="white-button guest-btn" onClick={this.onLoginGuest}>
+						<button type="button" className="white-button guest-btn" onClick={this.onLoginGuest}>
 							ВОЙТИ КАК ГОСТЬ
 						</button>
 
-						<Link className="reg-link" to="/registration">Зарегистрироваться</Link>
+						<Link className="reg-link" to="/registration">
+							Зарегистрироваться
+						</Link>
 
+						{t("Introduction")}
 					</div>
-					<Link className="passrec-link" to="/password-recovery">забыли пароль?</Link>
-
+					<Link className="passrec-link" to="/password-recovery">
+						забыли пароль?
+					</Link>
 				</form>
 			</div>
 		);
@@ -129,15 +146,10 @@ class Login extends React.Component {
 }
 
 class LoginContainer extends React.Component {
-
 	render() {
-
-		return (
-			<Login {...this.props} />
-		);
+		return <Login {...this.props} />;
 	}
 }
-
 
 const mapStateToProps = ({ user }) => {
 	return {
@@ -152,7 +164,6 @@ const mapDispatchToProps = (dispatch, { apiService }) => {
 	};
 };
 
-export default compose(
-	withApiService(),
-	connect(mapStateToProps, mapDispatchToProps)
-)(LoginContainer);
+export default withTranslation()(
+	compose(withApiService(), connect(mapStateToProps, mapDispatchToProps))(LoginContainer)
+);
