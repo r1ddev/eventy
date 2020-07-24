2be5d2b4b2acb6158726c804a89f04fa45e0b748
RidDev <evgeniidubenyuk87@gmail.com>
2020-07-24 23:20:57 +0300

    translate registration

--- src/pages/registration-acception/registration-acception.js
+++ src/pages/registration-acception/registration-acception.js
@@ -5,56 +5,51 @@ import { compose } from "../../utils";
 
 import "./registration-acception.scss";
 import { Link } from "react-router-dom";
-import ErrorIndicator from '../../components/error-indicator'
+import ErrorIndicator from "../../components/error-indicator";
+
+import { withTranslation } from "react-i18next";
 
 class RegistrationAcception extends React.Component {
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.props.history.push("/login");
+  };
 
-	onSubmit = (e) => {
-		e.preventDefault();
-		this.props.history.push("/login")
-	}
-
-	render() {
-		return (
-			<div id="registration-acception">
-				<form onSubmit={this.onSubmit} className="registration-acception-form">
-					<div className="registration-acception-form--wrapper">
-						<div className="registration-acception-form--caption">
-							Вам на почту пришла ссылка,
-							пожалуйста перейдите по ней, чтобы завершить регистрацию
-						</div>
-
-					</div>
-					<button className="white-button login-btn">ВОЙТИ</button>
-
-				</form>
-			</div>
-		);
-	}
+  render() {
+    const t = this.props.t;
+    return (
+      <div id="registration-acception">
+        <form onSubmit={this.onSubmit} className="registration-acception-form">
+          <div className="registration-acception-form--wrapper">
+            <div className="registration-acception-form--caption">
+              {t("Вам на почту пришла ссылка, пожалуйста перейдите по ней, чтобы завершить регистрацию")}
+            </div>
+          </div>
+          <button className="white-button login-btn">{t("ВОЙТИ")}</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 class RegistrationAcceptionContainer extends React.Component {
-
-	render() {
-
-		return (
-			<RegistrationAcception {...this.props} />
-		);
-	}
+  render() {
+    return <RegistrationAcception {...this.props} />;
+  }
 }
 
-
 const mapStateToProps = ({ user }) => {
-	return {
-		user,
-	};
+  return {
+    user,
+  };
 };
 
 const mapDispatchToProps = (dispatch, { apiService }) => {
-	return {};
+  return {};
 };
 
 export default compose(
-	withApiService(),
-	connect(mapStateToProps, mapDispatchToProps)
+  withTranslation(),
+  withApiService(),
+  connect(mapStateToProps, mapDispatchToProps)
 )(RegistrationAcceptionContainer);
