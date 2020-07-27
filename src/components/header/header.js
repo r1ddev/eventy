74b10bbf71ff210df6fb6850e2bf2c32c9f09963
ekivoka <sonya.sesofa@gmail.com>
2020-07-27 22:06:40 +0300

    up

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,29 +1,21 @@
-<<<<<<< HEAD
 import React from 'react';
 import './header.scss'
 import { Link } from 'react-router-dom';
 import api from './../../js/api';
 import LangChecker from '../lang-checker';
-=======
-import React from "react";
-import "./header.scss";
-import { Link } from "react-router-dom";
-import api from "./../../js/api";
->>>>>>> 5323227c2b5bb6010170c22e189aecbe07070aef
 
 import { Trans, withTranslation } from "react-i18next";
 
-<<<<<<< HEAD
-	state = {}
+class Header extends React.Component {
+	state = {};
 
 	render() {
+		const { data, className, t } = this.props;
 
-		const { data, className } = this.props;
-
-		let userAvatar = require("../../images/default-avatar.svg")
+		let userAvatar = require("../../images/default-avatar.svg");
 		if (data) {
 			if (data.avatar) {
-				userAvatar = api.auth.getAvatarLocation() + data.avatar
+				userAvatar = api.auth.getAvatarLocation() + data.avatar;
 			}
 		}
 
@@ -31,66 +23,25 @@ import { Trans, withTranslation } from "react-i18next";
 			<div className={className + " header profile-header"}>
 				<div className="container">
 					<div className="row">
-						<div className="col d-flex align-items-center">
-							{this.props.children && this.props.children[0]}
-						</div>
+						<div className="col d-flex align-items-center">{this.props.children && this.props.children[0]}</div>
 
 						<div className="col-md-auto profile row m-0">
-							{this.props.children && this.props.children[1]}
-							<div className="d-flex col-auto align-items-center">
+							<div className="col d-flex align-items-center p-0">
+								<Link to="/messages/5" className="action-link">
+									<Trans t={t}>Связь с организаторами</Trans>
+								</Link>
 							</div>
 							<div className="col-auto ava">
 								<Link to="/profile">
-									<img
-										src={userAvatar}
-										alt=""
-									/>
+									<img src={userAvatar} alt="" />
 								</Link>
 							</div>
 						</div>
 					</div>
 				</div>
 			</div>
-		)
+		);
 	}
-=======
-class Header extends React.Component {
-  state = {};
-
-  render() {
-    const { data, className, t } = this.props;
-
-    let userAvatar = require("../../images/default-avatar.svg");
-    if (data) {
-      if (data.avatar) {
-        userAvatar = api.auth.getAvatarLocation() + data.avatar;
-      }
-    }
-
-    return (
-      <div className={className + " header profile-header"}>
-        <div className="container">
-          <div className="row">
-            <div className="col d-flex align-items-center">{this.props.children && this.props.children[0]}</div>
-
-            <div className="col-md-auto profile row m-0">
-              <div className="col d-flex align-items-center p-0">
-                <Link to="/messages/5" className="action-link">
-                  <Trans t={t}>Связь с организаторами</Trans>
-                </Link>
-              </div>
-              <div className="col-auto ava">
-                <Link to="/profile">
-                  <img src={userAvatar} alt="" />
-                </Link>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
->>>>>>> 5323227c2b5bb6010170c22e189aecbe07070aef
 }
 
 export default withTranslation()(Header);
