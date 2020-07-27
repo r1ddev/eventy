1ff840c2202021d4c11656936b0fd76c954c0abe
RidDev <evgeniidubenyuk87@gmail.com>
2020-07-27 22:13:30 +0300

    add translations

--- src/utils/i18n.js
+++ src/utils/i18n.js
@@ -2,65 +2,116 @@ import i18n from "i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 
 i18n.use(LanguageDetector).init({
-	// we init with resources
-	resources: {
-		ru: {
-			translations: {
-				Авторизация: "Авторизация",
-				Пароль: "Пароль",
-				ВОЙТИ: "ВОЙТИ",
-				"ВОЙТИ КАК ГОСТЬ": "ВОЙТИ КАК ГОСТЬ",
-				Зарегистрироваться: "Зарегистрироваться",
-				"забыли пароль?": "забыли пароль?",
-				"Пользователя с этими данными не существует": "Пользователя с этими данными не существует",
+  // we init with resources
+  resources: {
+    ru: {
+      translations: {
+        Авторизация: "Авторизация",
+        Пароль: "Пароль",
+        ВОЙТИ: "ВОЙТИ",
+        "ВОЙТИ КАК ГОСТЬ": "ВОЙТИ КАК ГОСТЬ",
+        Зарегистрироваться: "Зарегистрироваться",
+        "забыли пароль?": "забыли пароль?",
+        "Пользователя с этими данными не существует": "Пользователя с этими данными не существует",
 
-				Регистрация: "Регистрация",
-				"Подтверждение пароля": "Подтверждение пароля",
-				"Пароли должны быть одинаковыми": "Пароли должны быть одинаковыми",
-				"Название компании": "Название компании",
-				РЕГИСТРАЦИЯ: "РЕГИСТРАЦИЯ",
-				Компания: "Компания",
+        Регистрация: "Регистрация",
+        "Подтверждение пароля": "Подтверждение пароля",
+        "Пароли должны быть одинаковыми": "Пароли должны быть одинаковыми",
+        "Название компании": "Название компании",
+        РЕГИСТРАЦИЯ: "РЕГИСТРАЦИЯ",
+        Компания: "Компания",
+        Агентство: "Агентство",
+        "Вам на почту пришла ссылка, пожалуйста перейдите по ней, чтобы завершить регистрацию":
+          "Вам на почту пришла ссылка, пожалуйста перейдите по ней, чтобы завершить регистрацию",
 
-				Лобби: "Лобби",
+        "Восстановление пароля": "Восстановление пароля",
+        ВОССТАНОВИТЬ: "ВОССТАНОВИТЬ",
 
-			},
-		},
-		en: {
-			translations: {
-				Авторизация: "Login",
-				Пароль: "Password",
-				ВОЙТИ: "SIGN IN",
-				"ВОЙТИ КАК ГОСТЬ": "SIGN IN AS GUEST",
-				Зарегистрироваться: "Register now",
-				"забыли пароль?": "Lost your Password?",
-				"Пользователя с этими данными не существует": "There is no user with this data",
+        Загрузка: "Загрузка",
+        Контакты: "Контакты",
+        "Изменить профиль": "Изменить профиль",
+        "Пользователь не найден": "Пользователь не найден",
+        Имя: "Имя",
+        Фамилия: "Фамилия",
+        Должность: "Должность",
+        Телефон: "Телефон",
+        "Ссылка на соц. сеть": "Ссылка на соц. сеть",
+        "Хочу ли я делиться контактами?": "Хочу ли я делиться контактами?",
+        "Изменить профиль": "Изменить профиль",
+        "Добро пожаловать на digital Stand-up Smit.Studio": `Добро пожаловать на <br />digital Stand-up Smit.Studio`,
 
-				Регистрация: "Register now",
-				"Подтверждение пароля": "Password confirmation",
-				"Пароли должны быть одинаковыми": "The password and confirm password must match",
-				"Название компании": "Company Name",
-				РЕГИСТРАЦИЯ: "CREATE ACCOUNT",
-				Компания: "Company",
-			},
-		},
-	},
-	fallbackLng: "en",
-	debug: true,
+        "Перейдите в нетворкинг и выберите человека, чтобы начать с ним общаться":
+          "Перейдите в нетворкинг и выберите человека, чтобы начать с ним общаться",
 
-	// have a common namespace used around the full app
-	ns: ["translations"],
-	defaultNS: "translations",
+        "Связь с организаторами": `Связь <br />с организаторами`,
 
-	keySeparator: false, // we use content as keys
+        "Открытые комнаты": "Открытые комнаты",
+        человек: "человек",
+      },
+    },
+    en: {
+      translations: {
+        Авторизация: "Login",
+        Пароль: "Password",
+        ВОЙТИ: "SIGN IN",
+        "ВОЙТИ КАК ГОСТЬ": "SIGN IN AS GUEST",
+        Зарегистрироваться: "Sign up now",
+        "забыли пароль?": "Forgot Password?",
+        "Пользователя с этими данными не существует": "There is no user with this data",
 
-	interpolation: {
-		escapeValue: false, // not needed for react!!
-		formatSeparator: ",",
-	},
+        Регистрация: "SIGN UP",
+        "Подтверждение пароля": "Password confirmation",
+        "Пароли должны быть одинаковыми": "The password and confirm password must match",
+        "Название компании": "Company Name",
+        РЕГИСТРАЦИЯ: "CREATE ACCOUNT",
+        Компания: "Company",
+        Агентство: "Agency",
+        "Вам на почту пришла ссылка, пожалуйста перейдите по ней, чтобы завершить регистрацию":
+          "You have received a link in your mail, please follow it to complete registration",
 
-	react: {
-		wait: true,
-	},
+        "Восстановление пароля": "Password recovery",
+        ВОССТАНОВИТЬ: "RESET PASSWORD",
+
+        Загрузка: "Loading",
+        Контакты: "Contacts",
+        "Изменить профиль": "Change profile",
+        "Пользователь не найден": "User is not found",
+        Имя: "Name",
+        Фамилия: "Surname",
+        Должность: "Position",
+        Телефон: "Phone",
+        "Ссылка на соц. сеть": "Link to social network",
+        "Хочу ли я делиться контактами?": "Do I want to share my contacts?",
+        "Изменить профиль": "Change profile",
+        "Добро пожаловать на digital Stand-up Smit.Studio": `Welcome to <br />digital Stand-up Smit.Studio`,
+
+        "Перейдите в нетворкинг и выберите человека, чтобы начать с ним общаться":
+          "Go to Networking and select a person to start chatting with",
+
+        "Связь с организаторами": `Contact <br />with organizers`,
+
+        "Открытые комнаты": "Open rooms",
+        человек: "people",
+      },
+    },
+  },
+  fallbackLng: "ru",
+  debug: true,
+
+  // have a common namespace used around the full app
+  ns: ["translations"],
+  defaultNS: "translations",
+
+  keySeparator: false, // we use content as keys
+
+  interpolation: {
+    escapeValue: false, // not needed for react!!
+    formatSeparator: ",",
+  },
+
+  react: {
+    wait: true,
+  },
 });
 
 export default i18n;
