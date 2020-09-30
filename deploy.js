e0754835e817f573f522b6d3ea8020dc8ffa31a0
Eugene Dubenyuk <evgeniidubenyuk87@gmail.com>
2020-09-30 23:02:56 +0300

    up deploy

--- deploy.js
+++ deploy.js
@@ -1,11 +1,14 @@
 const Deployer = require('ssh-deploy-release');
 
+let isWin = process.argv.filter(a => a == "-win").length > 0
+let privateKey = isWin ? "K:\\key\\riddev_pk" : "/home/zdsgf/.ssh/riddev_pk"
+
 const options = {
 	localPath: './build',
 	host: '116.203.213.27',
 	username: 'root',
 	password: 'root',
-	privateKeyFile: '/home/zdsgf/.ssh/riddev_pk',
+	privateKeyFile: privateKey,
 	deployPath: '/var/www/html/front/',
 };
 
