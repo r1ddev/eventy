5f04d56c9b4d7fe4ef65ba267980f6f9b5f02143
Eugene Dubenyuk <evgeniidubenyuk87@gmail.com>
2020-10-02 15:45:40 +0300

    conversation chat

--- src/pages/conversations-room/conversations-room.js
+++ src/pages/conversations-room/conversations-room.js
@@ -9,188 +9,359 @@ import Header from "../../components/header";
 import Spinner from "../../components/spinner";
 import NoPermissions from "../../components/no-permissions";
 import { fetchUser } from "../../actions/user-actions";
-import { conversationRoomsLoading, conversationRoomsLoaded } from "../../actions/conversations-actions";
+import {
+	conversationRoomsLoading,
+	conversationRoomsLoaded,
+} from "../../actions/conversations-actions";
+
+import { fetchMessages, updateMessages, fetchAddMessage } from "../../actions/chat-actions";
 import api from "../../js/api";
 import { isMobile } from "react-device-detect";
 
-import posed, { PoseGroup } from 'react-pose';
+import { withTranslation } from "react-i18next";
 
-const Item = posed.div({
-  // preEnter: {
-  //   opacity: 0,
-  //   transition: { type: "spring" }
-  // },
-  // enter: {
-  //   opacity: 1,
-  //   transition: { type: "spring" },
-  //   delay: ({ i }) => i * 200,
-  // },
-  // exit: {
-  //   opacity: 0,
-  //   transition: { type: "spring" }
-  // },
+import posed, { PoseGroup } from "react-pose";
+import ScenesChat from "../../components/scenes-chat";
+import ErrorIndicator from "../../components/error-indicator";
 
-  preEnter: {
-    scale: 1,
-    opacity: 0,
-    x: 100
-  },
+const Modal = posed.div({
   enter: {
-    delay: ({ i }) => i * 50,
-    scale: 1,
+    y: 0,
     opacity: 1,
-    x: 0
+    delay: 300,
+    transition: {
+      y: { type: 'spring', stiffness: 1000, damping: 15 },
+      default: { duration: 300 }
+    }
   },
   exit: {
-    delay: ({ i }) => i * 25,
+    y: 50,
     opacity: 0,
-    x: -100
+    transition: { duration: 150 }
   }
 });
+
+const Shade = posed.div({
+  enter: { opacity: 1 },
+  exit: { opacity: 0 }
+});
+
+const Item = posed.div({
+	// preEnter: {
+	//   opacity: 0,
+	//   transition: { type: "spring" }
+	// },
+	// enter: {
+	//   opacity: 1,
+	//   transition: { type: "spring" },
+	//   delay: ({ i }) => i * 200,
+	// },
+	// exit: {
+	//   opacity: 0,
+	//   transition: { type: "spring" }
+	// },
+
+	preEnter: {
+		scale: 1,
+		opacity: 0,
+		x: 100,
+	},
+	enter: {
+		delay: ({ i }) => i * 50,
+		scale: 1,
+		opacity: 1,
+		x: 0,
+	},
+	exit: {
+		delay: ({ i }) => i * 25,
+		opacity: 0,
+		x: -100,
+	},
+});
+
+
 class СonversationsRoom extends React.Component {
-  constructor() {
+	constructor() {
     super();
-
-    this.timeout = undefined;
+    
+    this.timeoutChat = undefined;
+		this.timeout = undefined;
     this.iframeRef = React.createRef();
-    this.state = {
-      room: undefined,
-      onlineUsers: []
-    };
-  }
+    
+		this.state = {
+      onlineUsers: [],
+      chatVisible: false
+		};
+	}
 
-  componentDidMount() {
-    let currentRoom = this.props.rooms.find((r) => r.room_id == this.props.match.params.room);
+	componentDidMount () {
+    if (this.props.room) {
+      
+      this.daily = DailyIframe.wrap(this.iframeRef.current);
+      this.daily.join({ url: this.props.room.url });
 
-    if (currentRoom) {
-      this.setState(
-        {
-          room: currentRoom,
-        },
-        () => {
-          this.daily = DailyIframe.wrap(this.iframeRef.current);
-          this.daily.join({ url: currentRoom.url });
-
-          this.updateRoomStatus();
-        }
-      );
+      this.updateRoomStatus();
+      
+      this.props.fetchMessages(this.props.room.chat_id)
+      this.updateMessages()
     }
   }
 
-  updateRoomStatus = () => {
-    api.account.conversations.updateRoomStatus(this.state.room.room_id).then(res => {
-      this.setState({
-        onlineUsers: res
-      })
-    }).catch(e => {
-      api.errorHandler(e, {
-        "access_denied": () => this.props.history.goBack()
-      })
+  updateMessages = () => {
+
+    let id = setTimeout(() => {
+        this.props.updateMessages(this.props.room.chat_id, this.props.chat.lastApiMessageId);
+        this.updateMessages();
+    }, this.props.timers.sceneChatTime)
+
+    this.timerId = id
+  }
+
+  sendMessage = (message, reply_id, replyAttachmentData) => {
+    console.log("this.props.user", this.props.user);
+    const { first_name, last_name, avatar, range } = this.props.user.data;
+
+    const date = new Date;
+
+    const mes = {
+        user_id: 1,
+        first_name: first_name,
+        last_name: last_name,
+        avatar: avatar,
+        range: range,
+        id: 1,
+        reply: replyAttachmentData,
+        reply_id: reply_id,
+        message: message,
+        time: `${('0' + date.getHours()).slice(-2)}:${('0' + date.getMinutes()).slice(-2)}`
+    }
+    this.props.fetchAddMessage(this.props.room.chat_id, mes, reply_id)
+    this.refs.scenesChat.onUpdate(true)
+  }
+
+	updateRoomStatus = () => {
+		api.account.conversations
+			.updateRoomStatus(this.props.room.room_id)
+			.then((res) => {
+				this.setState({
+					onlineUsers: res,
+				});
+			})
+			.catch((e) => {
+				api.errorHandler(e, {
+					access_denied: () => {
+            this.props.history.goBack();
+            ErrorIndicator("Комната недоступна")
+          },
+				});
+			});
+
+		this.timeout = setTimeout(() => {
+			this.updateRoomStatus();
+		}, this.props.timers.conversationsTimer);
+  };
+
+  toggleChat = () => {
+    this.setState({
+      chatVisible: !this.state.chatVisible
+    }, () => {
+      console.log(this.state.chatVisible);
     })
 
-    this.timeout = setTimeout(() => {
-      this.updateRoomStatus();
-    }, this.props.timers.conversationsTimer);
-  };
-
-  componentWillUnmount() {
-    clearTimeout(this.timeout);
+    
   }
-
-  render() {
-    const { data } = this.props.user;
-
-    return (
-      <div id="conversations-room">
-        {!isMobile && (
-          <Header data={data}>
-            <></>
-          </Header>
-        )}
-
-        <div className="container pt-3">
-          <div className="row">
-            <div className="col-lg-9">
-              {this.state.room && (
-                <iframe
-                  className="video"
-                  title="video call iframe"
-                  ref={this.iframeRef}
-                  allow="camera; microphone; fullscreen"></iframe>
-              )}
-            </div>
-            <div className="col-lg-3">
-
-              <div className="online-list-wrap">
-                <div className="title">Пользователи онлайн:</div>
-                <div className="online-list">
-                  <PoseGroup animateOnMount preEnterPose="preEnter">
-                    {
-                      this.state.onlineUsers.map(user => (
-                        <Item key={user.id}>
-                          <a key={user.id} href={`/profile/${user.id}`} className="link" target="_blank">
-                            <div className="row m-0">
-                              <div className="col-auto p-0">
-                                <div className="avatar">
-                                  <img src={api.auth.getAvatarLocation() + user.avatar} alt=""/>
-                                </div>
-                              </div>
-                              <div className="col username"><span>{`${user.first_name} ${user.last_name}`}</span></div>
-                            </div>
-                          </a>
-                        </Item>
-                      ))
-                    }
-                  </PoseGroup>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
-}
-class СonversationsRoomContainer extends React.Component {
-  componentDidMount() {
-    if (!this.props.conversations.isLoaded) {
-      api.account.conversations.getRooms().then((res) => {
-        this.props.conversationRoomsLoaded(res.rooms);
-      });
+  
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps.chat.updateLoading != this.props.chat.updateLoading && !this.props.chat.updateLoading) {
+      if (this.refs.scenesChat)
+        this.refs.scenesChat.onUpdate()
     }
 
+    if (prevProps.chat.loading != this.props.chat.loading && !this.props.chat.loading) {
+      if (this.refs.scenesChat)
+        this.refs.scenesChat.onUpdate(true)
+    }
+
+    if (prevState.chatVisible != this.state.chatVisible) {
+      if (this.refs.scenesChat)
+        this.refs.scenesChat.onUpdate(true)
+    }
+  }
+
+	componentWillUnmount() {
+		clearTimeout(this.timeout);
+		clearTimeout(this.timeoutChat);
+	}
+
+	render() {
+		const { data } = this.props.user;
+    const t = this.props.t;
+    
+    const { messages, loading, error } = this.props.chat;
+
+		return (
+			<div id="conversations-room">
+				{!isMobile && (
+					<Header data={data}>
+						<></>
+					</Header>
+				)}
+
+				<div className="container pt-3">
+					<div className="row wrap">
+						<div className="col-lg-9 left">
+							{this.props.room && (
+								<iframe
+									className="video"
+									title="video call iframe"
+									ref={this.iframeRef}
+									allow="camera; microphone; fullscreen"></iframe>
+							)}
+						</div>
+						<div className="col-lg-3 right">
+              <div className="chat-open-btn-wrap">
+                <button onClick={this.toggleChat} className="btn chat-open-btn"></button>
+              </div>
+              
+							<div className="online-list-wrap">
+								<div className="title">Пользователи онлайн:</div>
+								<div className="online-list">
+									<PoseGroup animateOnMount preEnterPose="preEnter">
+										{this.state.onlineUsers.map((user) => (
+											<Item key={user.id}>
+												<a
+													key={user.id}
+													href={`/profile/${user.id}`}
+													className="link"
+													target="_blank">
+													<div className="row m-0">
+														<div className="col-auto p-0">
+															<div className="avatar">
+																<img
+																	src={
+																		api.auth.getAvatarLocation() +
+																		user.avatar
+																	}
+																	alt=""
+																/>
+															</div>
+														</div>
+														<div className="col username">
+															<span>{`${user.first_name} ${user.last_name}`}</span>
+														</div>
+													</div>
+												</a>
+											</Item>
+										))}
+									</PoseGroup>
+								</div>
+							</div>
+              
+              {!this.state.chatVisible && 
+                <ScenesChat
+                  t={t}
+                  loading={loading}
+                  messages={messages}
+                  sendMessage={this.sendMessage}
+                  isPrivate={true}
+                  ref="scenesChat"
+                />
+              }
+						</div>
+					</div>
+				</div>
+
+        <PoseGroup>
+          {this.state.chatVisible && [
+            // If animating more than one child, each needs a `key`
+            <Shade key="shade" className="chat-shade" />,
+            <Modal key="modal" className="chat-modal-wrap">
+              <div className="chat-modal">
+                <div className="modal-close-btn" onClick={this.toggleChat}></div>
+                <div className="chat-modal-messages">
+                  <ScenesChat
+                    t={t}
+                    loading={loading}
+                    messages={messages}
+                    sendMessage={this.sendMessage}
+                    isPrivate={true}
+                    ref="scenesChat"
+                  />
+                </div>
+                
+              </div>
+            </Modal>
+          ]}
+        </PoseGroup>
+			</div>
+		);
+	}
+}
+class СonversationsRoomContainer extends React.Component {
+  state = {
+    room: undefined,
+  }
+
+	componentDidMount() {
     this.props.fetchUser();
+    this.loadRooms()
+  }
+  
+  loadRooms = async () => {
+    if (!this.props.conversations.isLoaded) {
+			let rooms = await api.account.conversations.getRooms()
+      this.props.conversationRoomsLoaded(rooms.rooms);
+    }
+
+    let currentRoom = this.props.conversations.rooms.find((r) => r.room_id == this.props.match.params.room);
+
+    if (currentRoom) {
+      this.setState({
+        room: currentRoom,
+      });
+    }
   }
 
-  render() {
-    let loading = true;
+	render() {
+		let loading = true;
     const { isLoaded: roomsLoading, rooms } = this.props.conversations;
+    const room = this.state.room;
 
-    loading = !roomsLoading;
+    loading = !roomsLoading || !room;
 
-    return (
-      <div style={{ height: "100%", width: "100%" }}>
-        {!loading && <СonversationsRoom {...this.props} rooms={rooms} />}
-        {loading && <Spinner big={1} />}
-      </div>
-    );
-  }
+		return (
+			<div style={{ height: "100%", width: "100%" }}>
+				{!loading && <СonversationsRoom {...this.props} room={room} />}
+				{loading && <Spinner big={1} />}
+			</div>
+		);
+	}
 }
 
-const mapStateToProps = ({ user, conversations, timers }) => {
-  return {
-    user,
-    conversations,
-    timers,
-  };
+const mapStateToProps = ({ chat, user, conversations, timers }) => {
+	return {
+		chat,
+		user,
+		conversations,
+		timers,
+	};
 };
 
 const mapDispatchToProps = (dispatch, { apiService }) => {
-  return {
-    conversationRoomsLoading: () => conversationRoomsLoading(dispatch),
-    conversationRoomsLoaded: (rooms) => conversationRoomsLoaded(dispatch)(rooms),
-    fetchUser: fetchUser(apiService, dispatch),
-  };
+	return {
+		conversationRoomsLoading: () => conversationRoomsLoading(dispatch),
+		conversationRoomsLoaded: (rooms) => conversationRoomsLoaded(dispatch)(rooms),
+		fetchUser: fetchUser(apiService, dispatch),
+
+		fetchMessages: (idChat) => fetchMessages(apiService, dispatch)(idChat),
+		updateMessages: (idChat, id) => updateMessages(apiService, dispatch)(idChat, id),
+		fetchAddMessage: (idChat, message, reply_id) =>
+			fetchAddMessage(apiService, dispatch)(idChat, message, reply_id),
+	};
 };
 
-export default compose(withApiService(), connect(mapStateToProps, mapDispatchToProps))(СonversationsRoomContainer);
+export default compose(
+	withTranslation(),
+	withApiService(),
+	connect(mapStateToProps, mapDispatchToProps)
+)(СonversationsRoomContainer);
