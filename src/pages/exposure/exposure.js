4a42e1947523ea62339cf783d952fd731f23f75e
Eugene Dubenyuk <evgeniidubenyuk87@gmail.com>
2020-10-05 18:07:26 +0300

    up

--- src/pages/exposure/exposure.js
+++ src/pages/exposure/exposure.js
@@ -38,7 +38,7 @@ class Exposure extends React.Component {
           className="card"
           onClick={() => this.props.postUrl(partner.link)}>
           <div className="row head m-0">
-            <div className="col-sm-5 logo flex-center">
+            <div className="col-sm-4 logo flex-center">
               <img src={partner.logo} alt="" />
             </div>
           </div>
