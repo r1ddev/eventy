47653ff9530760a98a0fe903729cd63ce44d15be
Eugene Dubenyuk <evgeniidubenyuk87@gmail.com>
2020-12-07 02:24:59 +0300

    reg-acception

--- src/pages/registration-acception/registration-acception.js
+++ src/pages/registration-acception/registration-acception.js
@@ -3,53 +3,68 @@ import withApiService from "../../components/hoc/with-api-service";
 import { connect } from "react-redux";
 import { compose } from "../../utils";
 
+import posed from 'react-pose';
 import "./registration-acception.scss";
 import { Link } from "react-router-dom";
 import ErrorIndicator from "../../components/error-indicator";
 
 import { withTranslation } from "react-i18next";
 
-class RegistrationAcception extends React.Component {
-  onSubmit = (e) => {
-    e.preventDefault();
-    this.props.history.push("/login");
-  };
+const Fade = posed.div({
+    hidden: {
+        opacity: 0
+    },
+    visible: {
+        opacity: 1
+    }
+})
 
-  render() {
-    const t = this.props.t;
-    return (
-      <div id="registration-acception">
-        <form onSubmit={this.onSubmit} className="registration-acception-form">
-          <div className="registration-acception-form--wrapper">
-            <div className="registration-acception-form--caption">
-              {t("Вам на почту пришла ссылка, пожалуйста перейдите по ней, чтобы завершить регистрацию")}
+class RegistrationAcception extends React.Component {
+
+    render() {
+        const t = this.props.t;
+        return (
+            <div id="registration-acception">
+                <Fade initialPose="hidden" pose="visible" className="flex-center w-100 h-100">
+                    <div className="registration-acception-container">
+
+                        <div className="form-item flex-center mt-0 mb-4">
+                            <img className="logo" src={require("../../images/avito_logo_rgb__work.png")} alt=""/>
+                        </div>
+
+                        <div className="registration-acception-container--caption">
+                            На указанный вами при регистрации почтовый адрес пришла ссылка подтверждения. Пожалуйста перейдите по ссылке в письме, чтобы завершить этап регистрации.
+                        </div>
+                        <div className="form-item flex-center mt-5">
+                            <Link className="e-button primary" to="/login">
+                                {t("ВОЙТИ")}
+                            </Link>
+                        </div>
+                    </div>
+                </Fade>
             </div>
-          </div>
-          <button className="white-button login-btn">{t("ВОЙТИ")}</button>
-        </form>
-      </div>
-    );
-  }
+        );
+    }
 }
 
 class RegistrationAcceptionContainer extends React.Component {
-  render() {
-    return <RegistrationAcception {...this.props} />;
-  }
+    render() {
+        return <RegistrationAcception {...this.props} />;
+    }
 }
 
 const mapStateToProps = ({ user }) => {
-  return {
-    user,
-  };
+    return {
+        user,
+    };
 };
 
 const mapDispatchToProps = (dispatch, { apiService }) => {
-  return {};
+    return {};
 };
 
 export default compose(
-  withTranslation(),
-  withApiService(),
-  connect(mapStateToProps, mapDispatchToProps)
+    withTranslation(),
+    withApiService(),
+    connect(mapStateToProps, mapDispatchToProps)
 )(RegistrationAcceptionContainer);
