c1f470642993dd031d63c6c4c769a326bfa380bd
Eugene Dubenyuk <evgeniidubenyuk87@gmail.com>
2021-03-01 23:24:39 +0300

    fix langs

--- src/pages/exposure/exposure.js
+++ src/pages/exposure/exposure.js
@@ -14,84 +14,86 @@ import parse from 'html-react-parser';
 
 class Exposure extends React.Component {
 
-  render() {
-    let list = this.props.list.map((partner) => {
-      return (
-        <Link
-          key={partner.id}
-          to={`exposure/${partner.id}`}
-          className="card">
-          <div className="row head m-0">
-            <div className="col-sm-4 logo flex-center">
-              <img src={partner.logo} alt="" />
-            </div>
-          </div>
-          <div className="desc">
-          {parse(partner.title_text)}
-          </div>
-        </Link>
-      );
-    });
+    render() {
+        let list = this.props.list.map((partner) => {
+            return (
+                <Link
+                    key={partner.id}
+                    to={`exposure/${partner.id}`}
+                    className="card">
+                    <div className="row head m-0">
+                        <div className="col-sm-4 logo flex-center">
+                            <img src={partner.logo} alt="" />
+                        </div>
+                    </div>
+                    <div className="desc">
+                    {parse(partner.title_text)}
+                    </div>
+                </Link>
+            );
+        });
 
-    return (
-      <div id="exposure">
-        {!isMobile && (
-          <Header className="fixed" data={this.props.user.data}>
-            <></>
-          </Header>
-        )}
-        <div className="container">
-          <div className="card-list">{list}</div>
-        </div>
-      </div>
-    );
-  }
+        return (
+            <div id="exposure">
+                {!isMobile && (
+                    <Header className="fixed" data={this.props.user.data}>
+                        <></>
+                    </Header>
+                )}
+                <div className="container">
+                    <div className="card-list">{list}</div>
+                </div>
+            </div>
+        );
+    }
 }
 
 class ExposureContainer extends React.Component {
-  
-  state = {
-    exposureList: [],
-    loading: true,
-  }
-
-  componentDidMount () {
-    api.account.exposure.getList().then(res => {
-      this.setState({
-        exposureList: res[Langs.getCurrentLang()],
-        loading: false
-      })
-    }).catch(e => [
-      api.errorHandler(e, {})
-    ])
-  }
-  render() {
-
-    const { exposureList, loading }  = this.state;
     
-    return (<div style={{ height: "100%", width: "100%" }}>
-      {!loading && <Exposure {...this.props} list={exposureList} />}
-      {loading && <Spinner big={1} center/>}
-    </div>)
-  }
+    state = {
+        exposureList: [],
+        loading: true,
+    }
+
+    componentDidMount () {
+        api.account.exposure.getList().then(res => {
+            console.log(Langs.getCurrentLang());
+            this.setState({
+                exposureList: res[Langs.getCurrentLang()] || [],
+                loading: false
+            })
+            console.log(Langs.getCurrentLang());
+        }).catch(e => [
+            api.errorHandler(e, {})
+        ])
+    }
+    render() {
+
+        const { exposureList, loading }  = this.state;
+        
+        return (<div style={{ height: "100%", width: "100%" }}>
+            {!loading && <Exposure {...this.props} list={exposureList} />}
+            {loading && <Spinner big={1} center/>}
+        </div>)
+    }
 }
 
 const mapStateToProps = ({ user }) => {
-  return {
-    user,
-  };
+    return {
+        user,
+    };
 };
 
 const mapDispatchToProps = (dispatch, { apiService }) => {
-  return {
-    postUrl: (url) => {
-      apiService.postUrl(url);
-    },
-  };
+    return {
+        postUrl: (url) => {
+            apiService.postUrl(url);
+        },
+    };
 };
 
 export default compose(
-  withTranslation(),
-  withApiService(),
-  connect(mapStateToProps, mapDispatchToProps)
+    withTranslation(),
+    withApiService(),
+    connect(mapStateToProps, mapDispatchToProps)
 )(ExposureContainer);
--- src/utils/lang.js
+++ src/utils/lang.js
@@ -4,6 +4,7 @@ import i18n from "./i18n";
 export default class Langs {
     static getCurrentLang () {
         if (~i18n.language.indexOf("en"))  return "en";
-        return "ua";
+        if (~i18n.language.indexOf("ua"))  return "ua";
+        return "ru";
     }
 }
